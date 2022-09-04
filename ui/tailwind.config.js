dd12f27261ac682fff168ebf6dc7b220c3d2fbfd
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-09-04 09:23:17 +0700

    chore: setup tailwindcss with daisyUI

---
+++ ui/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#2563eb",
+          // "primary-focus": "#3b82f6",
+        },
+      },
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
