ca7f917c2cb0a7a9d7f1f54203f48dce614a43b8
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-10-08 19:08:25 +0700

    feat: [server] re-create access token when access token expired

--- server/src/middleware/deserializeUser.ts
+++ server/src/middleware/deserializeUser.ts
@@ -1,23 +1,50 @@
 import { get } from "lodash";
 import { RequestHandler } from "express";
+import config from "config";
 import { verifyJwt } from "../utils/jwt.utils";
 import logger from "../utils/logger.utils";
 import { reIssueAccessToken } from "../services/session.service";
 
 const deserializeUser: RequestHandler = async (req, res, next) => {
   try {
-    const accessToken = get(req, "headers.authorization", "").replace(
-      /^Bearer\s/,
-      ""
-    );
+    const accessToken =
+      get(req, "cookies.accessToken") ||
+      get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
 
-    const refreshToken = get(req, "headers.x-refresh");
+    const refreshToken =
+      get(req, "cookies.refreshToken") || get(req, "headers.x-refresh");
 
     if (!accessToken) {
+      const { decoded: decodedRefresh } = verifyJwt(refreshToken);
+
+      if (!decodedRefresh) {
+        logger.info("Refresh token not found or expired!");
+        return next();
+      }
+
+      const newAccessToken = await reIssueAccessToken({ refreshToken });
+
+      if (newAccessToken) {
+        res.setHeader("x-access-token", newAccessToken);
+        res.cookie("accessToken", newAccessToken, {
+          maxAge: 900000, // 15 mins
+          httpOnly: true,
+          domain: config.get("domain"),
+          path: "/",
+          sameSite: "strict",
+          secure: false,
+        });
+      }
+
+      logger.info("No access found and generate new access token");
+      const result = verifyJwt(newAccessToken || "");
+
+      res.locals.user = result!.decoded;
       return next();
     }
 
     const { decoded, expired } = verifyJwt(accessToken);
+
     if (decoded) {
       res.locals.user = decoded;
       return next();
@@ -28,8 +55,16 @@ const deserializeUser: RequestHandler = async (req, res, next) => {
 
       if (newAccessToken) {
         res.setHeader("x-access-token", newAccessToken);
+        res.cookie("accessToken", newAccessToken, {
+          maxAge: 900000, // 15 mins
+          httpOnly: true,
+          domain: config.get("domain"),
+          path: "/",
+          sameSite: "strict",
+          secure: false,
+        });
       }
-      logger.info("Create new access token");
+      logger.info("Expired access token and generate new access token");
       const result = verifyJwt(newAccessToken || "");
 
       res.locals.user = result!.decoded;
--- server/src/middleware/requiredUser.ts
+++ server/src/middleware/requiredUser.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 
 function requiredUser(req: Request, res: Response, next: NextFunction) {
   const user = res.locals.user;
-
   if (!user) {
     return res.status(403).json({
       message: "Not authorized!",
